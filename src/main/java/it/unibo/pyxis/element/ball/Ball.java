b6e66a98f2c04f0514d5e2f0ba6368cbbf3f8a5a
Luca Rubboli <luca.rubboli2@studio.unibo.it>
2021-04-20 00:24:58 +0200

    Modified Ball Interface

--- src/main/java/it/unibo/pyxis/element/ball/Ball.java
+++ src/main/java/it/unibo/pyxis/element/ball/Ball.java
@@ -1,12 +1,15 @@
 package it.unibo.pyxis.element.ball;
 
 import it.unibo.pyxis.element.Element;
+import it.unibo.pyxis.element.brick.Brick;
+import it.unibo.pyxis.element.pad.Pad;
+import it.unibo.pyxis.event.notify.CollisionEvent;
 
 public interface Ball extends Element {
 
-    void handleBrickCollision();
+    void handleBrickCollision(CollisionEvent<Brick> collision);
 
-    void handlePadCollision();
+    void handlePadCollision(CollisionEvent<Pad> collision);
 
     void setStatus();
 
