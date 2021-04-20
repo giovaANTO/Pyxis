1c63b4e621f6241aeb7754ba27435f49fe8955ff
Luca Rubboli <luca.rubboli2@studio.unibo.it>
2021-04-21 00:40:31 +0200

    Add AbstractElement, BallImpl, ElementTest

---
+++ src/main/java/it/unibo/pyxis/element/ball/BallImpl.java
@@ -0,0 +1,29 @@
+package it.unibo.pyxis.element.ball;
+
+import it.unibo.pyxis.element.AbstractElement;
+import it.unibo.pyxis.element.brick.Brick;
+import it.unibo.pyxis.element.pad.Pad;
+import it.unibo.pyxis.event.notify.CollisionEvent;
+
+public class BallImpl extends AbstractElement implements Ball {
+
+    @Override
+    public void handleBrickCollision(CollisionEvent<Brick> collisionEvent) {
+
+    }
+
+    @Override
+    public void handlePadCollision(CollisionEvent<Pad> collisionEvent) {
+
+    }
+
+    @Override
+    public void setStatus() {
+
+    }
+
+    @Override
+    public void setPace() {
+
+    }
+}
---
+++ src/test/java/it/unibo/pyxis/element/ToTestElement.java
@@ -0,0 +1,4 @@
+package it.unibo.pyxis.element;
+
+public class ToTestElement extends AbstractElement{
+}
