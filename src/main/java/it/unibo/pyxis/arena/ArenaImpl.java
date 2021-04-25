04feec4f3a98cc982f081f22659349254f78dbad
RicaBiz <biliardelli@hotmail.it>
2021-04-25 22:12:12 +0200

    Added ArenaImpl class and fixed comments on Arena

---
+++ src/main/java/it/unibo/pyxis/arena/ArenaImpl.java
@@ -0,0 +1,79 @@
+package it.unibo.pyxis.arena;
+
+import java.util.stream.Stream;
+
+import it.unibo.pyxis.util.Coord;
+
+public class ArenaImpl implements Arena {
+
+
+    public void loadConfigurationFile() {
+
+    }
+
+
+    public void update() {
+
+    }
+
+
+    public void movePad() {
+
+    }
+
+
+    public void handleBrickDestruction(Coord brick) {
+
+    }
+
+
+    public void spawnPowerup() {
+
+    }
+
+
+    public void handlePowerupActivation() {
+
+    }
+
+
+    public boolean isArenaClear() {
+        return false;
+    }
+
+    
+    public void addElement() {
+
+    }
+
+
+    public void getLevel() {
+
+    }
+
+
+    public void getDimensions() {
+
+    }
+
+
+    public void setDimensions() {
+
+    }
+
+
+    public Stream<Object> getBallStream() {
+        return null;
+    }
+
+
+    public Stream<Object> getBrickStream() {
+        return null;
+    }
+
+
+    public void getPad() {
+
+    }
+
+}
