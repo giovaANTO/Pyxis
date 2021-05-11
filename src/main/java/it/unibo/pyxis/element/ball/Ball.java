7c69a4d60d99306166c53329189befe6dadf9904
Luca Rubboli <luca.rubboli2@studio.unibo.it>
2021-05-11 21:59:42 +0200

    Modified Ball, BallImpl and BallType

--- src/main/java/it/unibo/pyxis/element/ball/Ball.java
+++ src/main/java/it/unibo/pyxis/element/ball/Ball.java
@@ -10,37 +10,39 @@ public interface Ball extends Element {
 
     /**
      * Handles the collision event between the ball and a brick.
-     * @param collisionEvent
+     * @param collisionEvent {@link CollisionEvent}
      */
     void handleBrickCollision(CollisionEvent<Brick> collisionEvent);
 
     /**
      * Handles the collision event between the ball and the pad.
-     * @param collisionEvent
+     * @param collisionEvent {@link CollisionEvent}
      */
     void handlePadCollision(CollisionEvent<Pad> collisionEvent);
 
     /**
      * Returns the ball's type.
      * @return
+     *              The {@link BallType}
      */
     BallType getType();
 
     /**
      * Returns the ball's pace.
      * @return
+     *              The {@link Vector}
      */
     Vector getPace();
 
     /**
      * Sets the ball's type.
-     * @param type
+     * @param type {@link BallType}
      */
     void setType(BallType type);
 
     /**
      * Sets the ball's pace.
-     * @param pace
+     * @param pace {@link Vector}
      */
     void setPace(Vector pace);
 }
