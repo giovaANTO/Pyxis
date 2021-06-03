e7fc9f3d417dd6c39e9021ef981e01269b3a333e
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2021-06-03 11:27:31 +0200

    Add comments in LevelLoader class

--- src/main/java/it/unibo/pyxis/model/level/loader/LevelLoaderImpl.java
+++ src/main/java/it/unibo/pyxis/model/level/loader/LevelLoaderImpl.java
@@ -80,18 +80,41 @@ public final class LevelLoaderImpl implements LevelLoader {
         return filePath;
     }
 
+    /**
+     * Create an {@link Arena} instance from a skeleton.
+     * @param skeleton
+     *                  An {@link ArenaSkeleton} object that contains the information about the {@link Arena}
+     *                  that should be created.
+     * @return
+     *                  An instance of {@link Arena}
+     */
     private Arena arenaFromSkeleton(final ArenaSkeleton skeleton) {
         final Arena outputArena = new ArenaImpl(new DimensionImpl(skeleton.getWidth(), skeleton.getHeight()));
         skeleton.getBrickSkeletonSet().forEach(bs -> outputArena.addBrick(this.brickFromSkeleton(bs)));
         return outputArena;
     }
 
+    /**
+     * Create a {@link Brick} instance from a skeleton.
+     * @param skeleton
+     *                A {@link BrickSkeleton} object that contains the information about the {@link Arena}
+     *                that should be created.
+     * @return
+     *                An instance of {@link Arena}
+     */
     private Brick brickFromSkeleton(final BrickSkeleton skeleton) {
         final Coord brickCoord = new CoordImpl(skeleton.getX(), skeleton.getY());
         final BrickType brickType = this.getBrickType(skeleton.getType());
         return new BrickImpl(brickType, brickCoord);
     }
 
+    /**
+     * Get a {@link BrickType} from a type string loaded in a skeleton.
+     * @param typeString
+     *                 A type string loaded from a skeleton
+     * @return
+     *                  A {@link BrickType}
+     */
     private BrickType getBrickType(final String typeString) {
         return Arrays.stream(BrickType.values())
                 .filter(t -> t.getTypeString().equals(typeString))
