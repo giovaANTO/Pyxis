e4c00381eb9014a4c3d4c022ee410d3b6aeab9d6
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2021-07-24 11:43:01 +0200

    Modify stage resolution based on screen dimension

--- src/main/java/it/unibo/pyxis/app/Launcher.java
+++ src/main/java/it/unibo/pyxis/app/Launcher.java
@@ -5,10 +5,20 @@ import it.unibo.pyxis.controller.linker.LinkerImpl;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+
 public final class Launcher extends Application {
+
+    private static final double WINDOW_SCALE_FACTOR = 1.5;
 
     @Override
     public void start(final Stage primaryStage) throws Exception {
+        GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+        float width = gd.getDisplayMode().getWidth();
+        float height = gd.getDisplayMode().getHeight();
+        primaryStage.setHeight(height / WINDOW_SCALE_FACTOR);
+        primaryStage.setWidth(width / WINDOW_SCALE_FACTOR);
         Linker linker = new LinkerImpl(primaryStage);
         linker.start();
     }
