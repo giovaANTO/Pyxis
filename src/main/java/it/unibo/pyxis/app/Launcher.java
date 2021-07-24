99e9dcb7121fb8906f96811d85a6fdc15d55791c
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2021-07-24 11:11:40 +0200

    Launch application from the pyxis main package

--- src/main/java/it/unibo/pyxis/app/Launcher.java
+++ src/main/java/it/unibo/pyxis/app/Launcher.java
@@ -1,4 +1,19 @@
 package it.unibo.pyxis.app;
 
-public class Launcher {
+import it.unibo.pyxis.controller.linker.Linker;
+import it.unibo.pyxis.controller.linker.LinkerImpl;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public final class Launcher extends Application {
+
+    @Override
+    public void start(final Stage primaryStage) throws Exception {
+        Linker linker = new LinkerImpl(primaryStage);
+        linker.start();
+    }
+
+    public static void run(final String[] args) {
+        launch(args);
+    }
 }
--- src/main/java/it/unibo/pyxis/app/Main.java
+++ src/main/java/it/unibo/pyxis/app/Main.java
@@ -3,6 +3,6 @@ package it.unibo.pyxis.app;
 
 public class Main {
     public static void main(final String[] args) {
-
+        Launcher.run(args);
     }
 }
