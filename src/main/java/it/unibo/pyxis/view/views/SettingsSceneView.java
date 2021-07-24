585a769eaba7e2e352645116dd930b9f81ad40d1
RicaBiz <biliardelli@hotmail.it>
2021-07-24 11:27:26 +0200

    Basic SettingsView Added

--- src/main/java/it/unibo/pyxis/view/views/SettingsSceneView.java
+++ src/main/java/it/unibo/pyxis/view/views/SettingsSceneView.java
@@ -1,9 +1,23 @@
 package it.unibo.pyxis.view.views;
 
+import it.unibo.pyxis.controller.controllers.SettingsSceneController;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+
 public class SettingsSceneView extends AbstractView {
 
-    @Override
-    public void init() {
+    @FXML
+    private AnchorPane menuPane;
+    @FXML
+    private Button backButton;
 
+    @Override
+    public final void init() {
+    }
+
+    public final void backToMainMenu() {
+        SettingsSceneController controller = (SettingsSceneController) this.getController();
+        controller.backToMainMenu();
     }
 }
