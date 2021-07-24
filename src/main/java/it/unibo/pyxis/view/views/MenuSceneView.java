87b3e40c3137f6219df8f39922078d6b0befae73
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2021-07-24 21:33:47 +0200

    Add responsive configuration to the menu

--- src/main/java/it/unibo/pyxis/view/views/MenuSceneView.java
+++ src/main/java/it/unibo/pyxis/view/views/MenuSceneView.java
@@ -2,22 +2,45 @@ package it.unibo.pyxis.view.views;
 
 import it.unibo.pyxis.controller.controllers.MenuSceneController;
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MenuSceneView extends AbstractJavaFXView {
 
-    @FXML
-    private AnchorPane menuPane;
-    @FXML
-    private Button newGameButton, settingsButton, levelsButton, quitButton;
-    @FXML
-    private ImageView menuImageView;
+    private static final Double SCALE_FACTOR = 1.5;
 
+    @FXML
+    private StackPane mainPane;
+    @FXML
+    private VBox vBox;
+    @FXML
+    private Button newGameButton;
+    @FXML
+    private Button settingsButton;
+    @FXML
+    private Button levelsButton;
+    @FXML
+    private Button quitButton;
+
+    @Override
+    public void initialize(final URL location, final ResourceBundle resources) {
+
+        vBox.prefWidthProperty().bind(mainPane.prefWidthProperty());
+        vBox.prefHeightProperty().bind(mainPane.prefHeightProperty());
+
+
+        newGameButton.setPrefWidth(mainPane.getPrefWidth() / SCALE_FACTOR);
+        settingsButton.setPrefWidth(mainPane.getPrefWidth() / SCALE_FACTOR);
+        levelsButton.setPrefWidth(mainPane.getPrefWidth() / SCALE_FACTOR);
+        quitButton.setPrefWidth(mainPane.getPrefWidth() / SCALE_FACTOR);
+
+        StackPane.setAlignment(vBox, Pos.CENTER);
+    }
 
     public final void startNewGame() {
         MenuSceneController controller = (MenuSceneController) this.getController();
@@ -36,8 +59,5 @@ public class MenuSceneView extends AbstractJavaFXView {
         controller.quit();
     }
 
-    @Override
-    public void initialize(final URL location, final ResourceBundle resources) {
 
-    }
 }
