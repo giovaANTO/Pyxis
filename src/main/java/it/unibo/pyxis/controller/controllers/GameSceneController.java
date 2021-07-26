c1c710bb6c036fe002e0e923338bdda2b950106b
RicaBiz <biliardelli@hotmail.it>
2021-07-26 14:42:05 +0200

    Refactored the initialize method

--- src/main/java/it/unibo/pyxis/controller/controllers/GameSceneController.java
+++ src/main/java/it/unibo/pyxis/controller/controllers/GameSceneController.java
@@ -9,23 +9,23 @@ import it.unibo.pyxis.model.element.pad.Pad;
 public class GameSceneController extends AbstractController {
 
     public final Set<Brick> getBricks() {
-        return this.getLinker().getGameState().getCurrentLevel().getArena().getBricks();
+        return this.getLevel().getArena().getBricks();
     }
 
     public final Set<Ball> getBalls() {
-        return this.getLinker().getGameState().getCurrentLevel().getArena().getBalls();
+        return this.getLevel().getArena().getBalls();
     }
 
     public final Pad getPad() {
-        return this.getLinker().getGameState().getCurrentLevel().getArena().getPad();
+        return this.getLevel().getArena().getPad();
     }
 
     public final double getArenaWidth() {
-        return this.getLinker().getGameState().getCurrentLevel().getArena().getDimension().getWidth();
+        return this.getLevel().getArena().getDimension().getWidth();
     }
 
     public final double getArenaHeight() {
-        return this.getLinker().getGameState().getCurrentLevel().getArena().getDimension().getHeight();
+        return this.getLevel().getArena().getDimension().getHeight();
     }
 
 }
--- src/main/java/it/unibo/pyxis/view/views/GameSceneView.java
+++ src/main/java/it/unibo/pyxis/view/views/GameSceneView.java
@@ -27,22 +27,15 @@ public final class GameSceneView extends AbstractJavaFXView<GameSceneController>
     }
 
     public void initialize(final URL location, final ResourceBundle resources) {
-        System.out.println(this.getController());
-        final GraphicsContext gc = canvas.getGraphicsContext2D();
-        gc.setFill(BLACK);
-        gc.fillRect(50, 50, 100, 100);
-    }
-
-    public void initBottone() {
-        final GameSceneController controller = (GameSceneController) this.getController();
+        final GameSceneController controller = this.getController();
         drawer = new GameCanvasDrawer(this.canvas.getGraphicsContext2D(),
                 this.canvas.getWidth() / controller.getArenaWidth(),
                 this.canvas.getHeight() / controller.getArenaHeight());
-        render();
     }
 
     public void render() {
-        final GameSceneController controller = (GameSceneController) this.getController();
+        System.out.println("K");
+        final GameSceneController controller = this.getController();
 
         controller.getBricks().stream().forEach(b -> drawer.fillBrick(b.getPosition(), b.getDimension(), b.getBrickType()));
         controller.getBalls().stream().forEach(b -> drawer.fillBall(b.getPosition(), b.getDimension(), b.getType()));
