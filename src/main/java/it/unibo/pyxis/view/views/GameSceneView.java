69011a456632a1a9a4404fe5b1400290e255b2da
RicaBiz <biliardelli@hotmail.it>
2021-07-28 10:55:43 +0200

    Added AspectRatio Feature for the Canvas

--- src/main/java/it/unibo/pyxis/view/views/GameSceneView.java
+++ src/main/java/it/unibo/pyxis/view/views/GameSceneView.java
@@ -1,15 +1,27 @@
 package it.unibo.pyxis.view.views;
 
 import it.unibo.pyxis.controller.controllers.GameSceneController;
+import javafx.beans.binding.Bindings;
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import it.unibo.pyxis.view.drawer.CanvasPropertyBinder;
 import it.unibo.pyxis.view.drawer.GameArenaDrawer;
 
 public final class GameSceneView extends AbstractJavaFXView<GameSceneController> implements RenderableView {
+
+    @FXML
+    private AnchorPane mainPane;
+
+    @FXML
+    private StackPane stackPane;
 
     @FXML
     private Canvas arenaCanvas;
@@ -18,13 +30,20 @@ public final class GameSceneView extends AbstractJavaFXView<GameSceneController>
     private Label currentLives, currentScore, currentLevel;
 
     private GameArenaDrawer drawer;
+    private CanvasPropertyBinder canvasToMainPaneBinder;
 
     public GameSceneView(final GameSceneController inputController) {
         super(inputController);
     }
 
     public void initialize(final URL location, final ResourceBundle resources) {
+        stackPane.prefHeightProperty().bind(mainPane.heightProperty());
+        stackPane.prefWidthProperty().bind(mainPane.widthProperty());
         this.drawer = new GameArenaDrawer(arenaCanvas.getGraphicsContext2D(), this.getController().getArenaDimension());
+        this.canvasToMainPaneBinder = new CanvasPropertyBinder(mainPane.widthProperty(), mainPane.heightProperty(),
+                mainPane.getPrefWidth(), mainPane.getPrefHeight(),
+                arenaCanvas.widthProperty(), arenaCanvas.heightProperty(),
+                arenaCanvas.getWidth(), arenaCanvas.getHeight());
     }
 
     public void back() {
@@ -33,12 +52,14 @@ public final class GameSceneView extends AbstractJavaFXView<GameSceneController>
 
     @Override
     public void render() {
-        this.drawer.clearCanvas();
+        canvasToMainPaneBinder.bindWithRatioToContainer();
         this.currentLives.setText(this.getController().getLives().toString());
         this.currentScore.setText(this.getController().getScore().toString());
+        this.drawer.clearCanvas();
         this.getController().getBricks().forEach(b -> this.drawer.fillBrick(b.getPosition(), b.getDimension(), b.getBrickType()));
         this.getController().getBalls().forEach(b -> this.drawer.fillBall(b.getPosition(), b.getDimension(), b.getType()));
         this.getController().getPowerups().forEach(p -> this.drawer.fillPowerup(p.getPosition(), p.getDimension(), p.getType()));
         this.drawer.fillPad(this.getController().getPad());
     }
+
 }
