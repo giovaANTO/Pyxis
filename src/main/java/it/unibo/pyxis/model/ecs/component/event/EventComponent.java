973a8d5a1fb9b315380163993c7721325c315341
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2021-08-03 17:06:07 +0200

    Add EventComponent interface

---
+++ src/main/java/it/unibo/pyxis/model/ecs/component/event/EventComponent.java
@@ -0,0 +1,7 @@
+package it.unibo.pyxis.model.ecs.component.event;
+
+import it.unibo.pyxis.model.ecs.component.Component;
+import it.unibo.pyxis.model.ecs.entity.Entity;
+
+public interface EventComponent<E extends Entity> extends Component<E> {
+}
