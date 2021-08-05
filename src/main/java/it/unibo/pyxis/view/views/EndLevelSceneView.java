a481ecb88effd68ff501049ba7a3b9a0756889f5
RicaBiz <biliardelli@hotmail.it>
2021-08-05 11:31:00 +0200

    new EndLevelScene added

--- src/main/java/it/unibo/pyxis/view/views/EndLevelSceneView.java
+++ src/main/java/it/unibo/pyxis/view/views/EndLevelSceneView.java
@@ -5,11 +5,17 @@ import java.util.ResourceBundle;
 import it.unibo.pyxis.controller.controllers.EndLevelSceneController;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 
 public final class EndLevelSceneView extends AbstractJavaFXView<EndLevelSceneController> {
 
     @FXML
-    private Button nextLevelButton, menuButton;
+    private StackPane mainPane;
+    @FXML
+    private VBox vBox;
+    @FXML
+    private Button nextLevelButton;
 
     public EndLevelSceneView(final EndLevelSceneController inputController) {
         super(inputController);
@@ -17,6 +23,9 @@ public final class EndLevelSceneView extends AbstractJavaFXView<EndLevelSceneCon
 
     @Override
     public void initialize(final URL location, final ResourceBundle resources) {
+        vBox.prefWidthProperty().bind(mainPane.prefWidthProperty());
+        vBox.prefHeightProperty().bind(mainPane.prefHeightProperty());
+
         this.nextLevelButton.setDisable(this.getController().disableNextLevelButton());
     }
 
